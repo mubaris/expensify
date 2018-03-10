d929384428a6dfb344eabbc5b850aa6f774792b7
Mubaris NK <mubarishassannk@gmail.com>
2018-03-10 11:27:41 +0530

    React Router init

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Hello </p>, document.getElementById('root'));
+const ExpenseDashboardPage = () => (
+    <div>
+        Hello There
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        Hello from Add Expense Page
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('root'));
