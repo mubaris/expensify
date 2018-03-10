3c8f0e9fe7d197aeec59f56acff1626c294b198b
Mubaris NK <mubarishassannk@gmail.com>
2018-03-10 11:02:28 +0530

    Boilerplate

---
+++ src/emoji.js
@@ -0,0 +1,11 @@
+import EmojiConvertor from 'emoji-js';
+
+const emoji = new EmojiConvertor();
+
+// emoji.img_sets.apple.path = 'https://apps.mubaris.com/filmoji/emoji-data/img-apple-64/';
+
+// emoji.img_sets.apple.sheet = 'https://apps.mubaris.com/filmoji/emoji-data/sheet_apple_64.png';
+
+const convertToEmoji = e => emoji.replace_unified(e);
+
+export default convertToEmoji;
